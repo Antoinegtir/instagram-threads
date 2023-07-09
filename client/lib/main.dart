68753ad53efc916ffc284e856571303959a5966a
Antoine Gonthier <antoine.gonthier@epitech.eu>
2023-07-10 00:23:36 +0200

    ✨ add main file of Threads

---
+++ client/lib/main.dart
@@ -0,0 +1,44 @@
+import 'package:camera/camera.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:threads/common/locator.dart';
+import 'package:threads/common/splash.dart';
+import 'package:threads/state/appState.dart';
+import 'package:threads/state/authState.dart';
+import 'package:provider/provider.dart';
+import 'package:threads/state/post.dart';
+import 'package:threads/state/searchState.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+List<CameraDescription> cameras = [];
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  cameras = await availableCameras();
+  await Firebase.initializeApp();
+  setupDependencies();
+  final sharedPreferences = await SharedPreferences.getInstance();
+  runApp(MyApp(
+    sharedPreferences: sharedPreferences,
+  ));
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key, required this.sharedPreferences}) : super(key: key);
+  final SharedPreferences sharedPreferences;
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider<AppStates>(create: (_) => AppStates()),
+        ChangeNotifierProvider<AuthState>(create: (_) => AuthState()),
+        ChangeNotifierProvider<PostState>(create: (_) => PostState()),
+        ChangeNotifierProvider<SearchState>(create: (_) => SearchState()),
+      ],
+      child: MaterialApp(
+          theme: ThemeData(brightness: Brightness.dark),
+          title: 'Threads',
+          debugShowCheckedModeBanner: false,
+          home: SplashPage()),
+    );
+  }
+}
