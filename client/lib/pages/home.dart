cae3d9e7b904ac11f2d5a5affbd4438604426727
Antoine Gonthier <antoine.gonthier@epitech.eu>
2023-07-10 00:24:40 +0200

    ✨ add home page

---
+++ client/lib/pages/home.dart
@@ -0,0 +1,185 @@
+import 'package:animate_do/animate_do.dart';
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter/services.dart';
+import 'package:iconsax/iconsax.dart';
+import 'package:provider/provider.dart';
+import 'package:threads/camera/camera.dart';
+import 'package:threads/model/post.dart';
+import 'package:threads/model/user.dart';
+import 'package:threads/pages/profile.dart';
+import 'package:threads/state/authState.dart';
+import 'package:threads/state/post.dart';
+import 'package:threads/state/searchState.dart';
+import 'package:threads/styles/color.dart';
+import 'package:threads/pages/myprofile.dart';
+import 'package:threads/widget/feedpost.dart';
+import 'package:threads/widget/gridpost.dart';
+import 'package:threads/widget/list.dart';
+import '../widget/custom/rippleButton.dart';
+import 'feed.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
+  late TabController _tabController;
+  ScrollController _scrollController = ScrollController();
+  bool _isScrolledDown = false;
+  bool _isGrid = false;
+
+  @override
+  void initState() {
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      initPosts();
+      initSearch();
+      initProfile();
+    });
+    _scrollController.addListener(_scrollListener);
+    _tabController = TabController(length: 3, vsync: this);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _scrollController.removeListener(_scrollListener);
+    _scrollController.dispose();
+    _tabController.dispose();
+    super.dispose();
+  }
+
+  void initSearch() {
+    var searchState = Provider.of<SearchState>(context, listen: false);
+    searchState.getDataFromDatabase();
+  }
+
+  void initProfile() {
+    var state = Provider.of<AuthState>(context, listen: false);
+    state.databaseInit();
+  }
+
+  void initPosts() {
+    var state = Provider.of<PostState>(context, listen: false);
+    state.databaseInit();
+    state.getDataFromDatabase();
+  }
+
+  void _scrollListener() {
+    if (_scrollController.position.userScrollDirection ==
+        ScrollDirection.reverse) {
+      setState(() {
+        _isScrolledDown = true;
+      });
+    } else if (_scrollController.position.userScrollDirection ==
+        ScrollDirection.forward) {
+      setState(() {
+        _isScrolledDown = false;
+      });
+    }
+  }
+
+  Future _bodyView() async {
+    if (_isGrid) {
+      setState(() {
+        _isGrid = false;
+      });
+    } else {
+      setState(() {
+        _isGrid = true;
+      });
+    }
+  }
+
+  int tab = 0;
+  @override
+  Widget build(BuildContext context) {
+    var authState = Provider.of<AuthState>(context, listen: false);
+    final state = Provider.of<SearchState>(context);
+
+    authState.getCurrentUser().then((value) {
+      setState(() {});
+    });
+    return Scaffold(
+        extendBody: true,
+        bottomNavigationBar: Container(
+            height: 150,
+            width: MediaQuery.of(context).size.width,
+            child: Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+              Icon(
+                Iconsax.home,
+                size: 30,
+                color: Colors.white,
+              ),
+              Container(
+                width: 40,
+              ),
+              Icon(
+                Iconsax.search_normal,
+                size: 30,
+                color: Colors.white,
+              ),
+              Container(
+                width: 40,
+              ),
+              Icon(
+                Iconsax.edit,
+                size: 30,
+                color: Colors.white,
+              ),
+              Container(
+                width: 40,
+              ),
+              Icon(
+                Iconsax.heart,
+                size: 30,
+                color: Colors.white,
+              ),
+              Container(
+                width: 40,
+              ),
+              GestureDetector(
+                  onTap: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: ((context) => MyProfilePage())));
+                  },
+                  child: Icon(
+                    CupertinoIcons.person,
+                    size: 30,
+                    color: Colors.white,
+                  ))
+            ])),
+        extendBodyBehindAppBar: true,
+        backgroundColor: Colors.black,
+        appBar: AppBar(
+          leading: Container(),
+          title: Image.asset(
+            "assets/thread.png",
+            height: 50,
+          ),
+          toolbarHeight: 37,
+          elevation: 0,
+          backgroundColor: Colors.transparent,
+        ),
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              width: MediaQuery.of(context).size.width,
+            ),
+            Text(
+              '🚧',
+              style: TextStyle(fontSize: 40),
+            )
+          ],
+        ));
+  }
+}
