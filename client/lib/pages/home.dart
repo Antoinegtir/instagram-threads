1ee40a826952c54b38ab6087b3b86c742365b225
Antoine Gonthier <antoine.gonthier@epitech.eu>
2023-07-12 22:50:28 +0200

    ✨ Add full homepage

--- client/lib/pages/home.dart
+++ client/lib/pages/home.dart
@@ -1,13 +1,14 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
 import 'package:iconsax/iconsax.dart';
-import 'package:lottie/lottie.dart';
 import 'package:provider/provider.dart';
+import 'package:threads/notification/notification.dart';
+import 'package:threads/pages/search.dart';
 import 'package:threads/state/authState.dart';
 import 'package:threads/state/post.dart';
 import 'package:threads/state/searchState.dart';
 import 'package:threads/pages/myprofile.dart';
+import 'feed.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -17,11 +18,6 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
-  late TabController _tabController;
-  ScrollController _scrollController = ScrollController();
-  bool _isScrolledDown = false;
-  bool _isGrid = false;
-
   @override
   void initState() {
     WidgetsBinding.instance.addPostFrameCallback((_) {
@@ -29,16 +25,11 @@ class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
       initSearch();
       initProfile();
     });
-    _scrollController.addListener(_scrollListener);
-    _tabController = TabController(length: 3, vsync: this);
     super.initState();
   }
 
   @override
   void dispose() {
-    _scrollController.removeListener(_scrollListener);
-    _scrollController.dispose();
-    _tabController.dispose();
     super.dispose();
   }
 
@@ -58,114 +49,68 @@ class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
     state.getDataFromDatabase();
   }
 
-  void _scrollListener() {
-    if (_scrollController.position.userScrollDirection ==
-        ScrollDirection.reverse) {
-      setState(() {
-        _isScrolledDown = true;
-      });
-    } else if (_scrollController.position.userScrollDirection ==
-        ScrollDirection.forward) {
-      setState(() {
-        _isScrolledDown = false;
-      });
-    }
+  Widget tabPage(int index) {
+    if (index == 0) return FeedPage();
+    if (index == 1) return SearchPage();
+    if (index == 2) return FeedPage();
+    if (index == 3) return NotificationPage();
+    if (index == 4) return MyProfilePage();
+    return FeedPage();
   }
 
-  Future _bodyView() async {
-    if (_isGrid) {
-      setState(() {
-        _isGrid = false;
-      });
-    } else {
-      setState(() {
-        _isGrid = true;
-      });
-    }
+  bool isSelected = false;
+  Widget iconBar(int tabCount, IconData icon) {
+    return GestureDetector(
+        onTap: () {
+          setState(() {
+            tab = tabCount;
+          });
+        },
+        child: Icon(
+          icon,
+          size: 30,
+          color: Colors.grey,
+        ));
+  }
+
+  Widget bottomNavBar() {
+    Widget separator = Container(
+      width: 40,
+    );
+    return Container(
+        color: Colors.black,
+        height: 90,
+        padding: EdgeInsets.only(bottom: 20),
+        width: MediaQuery.of(context).size.width,
+        child: Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+          iconBar(0, Iconsax.home),
+          separator,
+          iconBar(1, Iconsax.search_normal),
+          separator,
+          iconBar(2, Iconsax.edit),
+          separator,
+          iconBar(3, Iconsax.heart),
+          separator,
+          iconBar(
+            4,
+            CupertinoIcons.person,
+          ),
+        ]));
   }
 
   int tab = 0;
+
   @override
   Widget build(BuildContext context) {
     var authState = Provider.of<AuthState>(context, listen: false);
-    final state = Provider.of<SearchState>(context);
-
     authState.getCurrentUser().then((value) {
       setState(() {});
     });
     return Scaffold(
         extendBody: true,
-        bottomNavigationBar: Container(
-            height: 150,
-            width: MediaQuery.of(context).size.width,
-            child: Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-              Icon(
-                Iconsax.home,
-                size: 30,
-                color: Colors.white,
-              ),
-              Container(
-                width: 40,
-              ),
-              Icon(
-                Iconsax.search_normal,
-                size: 30,
-                color: Colors.white,
-              ),
-              Container(
-                width: 40,
-              ),
-              Icon(
-                Iconsax.edit,
-                size: 30,
-                color: Colors.white,
-              ),
-              Container(
-                width: 40,
-              ),
-              Icon(
-                Iconsax.heart,
-                size: 30,
-                color: Colors.white,
-              ),
-              Container(
-                width: 40,
-              ),
-              GestureDetector(
-                  onTap: () {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: ((context) => MyProfilePage())));
-                  },
-                  child: Icon(
-                    CupertinoIcons.person,
-                    size: 30,
-                    color: Colors.white,
-                  ))
-            ])),
+        bottomNavigationBar: bottomNavBar(),
         extendBodyBehindAppBar: true,
         backgroundColor: Colors.black,
-        appBar: AppBar(
-          leading: Container(),
-          title: Lottie.network(
-              "https://assets3.lottiefiles.com/packages/lf20_Ht77kFLXYw.json",height: 50),
-          toolbarHeight: 37,
-          elevation: 0,
-          backgroundColor: Colors.transparent,
-        ),
-        body: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Container(
-              width: MediaQuery.of(context).size.width,
-            ),
-            Text(
-              '🚧',
-              style: TextStyle(fontSize: 40),
-            )
-          ],
-        ));
+        body: tabPage(tab));
   }
 }
