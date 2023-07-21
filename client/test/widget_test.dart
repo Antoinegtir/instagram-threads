7c8acbba13826500b49b710621d3a3ef0f57c2ba
Antoine • Developer <antoine.gonthier@epitech.eu>
2023-07-21 06:40:41 +0200

    🧪 remove unit test

--- client/test/widget_test.dart
+++ client/test/widget_test.dart
@@ -5,9 +5,11 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
+// ignore_for_file: unused_import
+
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  
+
 }
