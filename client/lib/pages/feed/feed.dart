13d77c3d12a0596a6dda6bb8724e6d567de76561
Antoine • Developer <antoine.gonthier@epitech.eu>
2023-07-21 18:11:12 +0200

    📑 introduce feed page

--- client/lib/pages/feed/feed.dart
+++ client/lib/pages/feed/feed.dart
@@ -1,6 +1,13 @@
 // ignore_for_file: must_be_immutable
+
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:lottie/lottie.dart';
+import 'package:provider/provider.dart';
+import 'package:threads/model/post.module.dart';
+import 'package:threads/state/auth.state.dart';
+import 'package:threads/state/post.state.dart';
+import 'package:threads/widget/feedpost.dart';
 
 class FeedPage extends StatefulWidget {
   const FeedPage({super.key});
@@ -10,22 +17,70 @@ class FeedPage extends StatefulWidget {
 }
 
 class _FeedPageState extends State<FeedPage> with TickerProviderStateMixin {
+  ScrollController _scrollController = ScrollController();
+  bool _isScrolledDown = false;
+  void _scrollListener() {
+    if (_scrollController.position.userScrollDirection ==
+        ScrollDirection.reverse) {
+      setState(() {
+        _isScrolledDown = true;
+      });
+    } else if (_scrollController.position.userScrollDirection ==
+        ScrollDirection.forward) {
+      setState(() {
+        _isScrolledDown = false;
+      });
+    }
+  }
+
+  @override
+  void initState() {
+    _scrollController.addListener(_scrollListener);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _scrollController.removeListener(_scrollListener);
+    _scrollController.dispose();
+    super.dispose();
+  }
+
   int currentIndex = 0;
   @override
   Widget build(BuildContext context) {
+    var authState = Provider.of<AuthState>(context, listen: false);
     return Scaffold(
-        extendBody: true,
-        backgroundColor: Colors.black,
-        appBar: AppBar(
-          leading: Container(),
-          centerTitle: true,
-          title: Lottie.network(
-              "https://assets3.lottiefiles.com/packages/lf20_Ht77kFLXYw.json",
-              height: 50),
-          toolbarHeight: 37,
-          elevation: 0,
-          backgroundColor: Colors.transparent,
-        ),
-        body: Container());
+      extendBody: true,
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        leading: Container(),
+        centerTitle: true,
+        title: Lottie.network(
+            "https://assets3.lottiefiles.com/packages/lf20_Ht77kFLXYw.json",
+            height: 50),
+        toolbarHeight: 37,
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+      ),
+      body: Consumer<PostState>(builder: (context, state, child) {
+        if (state.isBusy)
+          return Container();
+        else
+          return Container(
+              height: MediaQuery.of(context).size.height,
+              width: MediaQuery.of(context).size.width,
+              child: ListView.builder(
+                  controller: _scrollController,
+                  itemCount:
+                      state.getPostList(authState.userModel)?.toList().length ??
+                          0,
+                  itemBuilder: (context, index) {
+                    return FeedPostWidget(
+                      postModel: state.getPostList(authState.userModel)![index],
+                    );
+                  }));
+      }),
+    );
   }
 }
