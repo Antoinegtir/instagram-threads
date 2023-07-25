6c738f50ee630739a46eb26598ae29a04b92f7f4
Antoine • Developer <antoine.gonthier@epitech.eu>
2023-07-25 10:00:32 +0200

    🐛 fix problem from feed

--- client/lib/widget/feedpost.dart
+++ client/lib/widget/feedpost.dart
@@ -1,5 +1,4 @@
 import 'package:cached_network_image/cached_network_image.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:iconsax/iconsax.dart';
 import 'package:threads/helper/utility.dart';
